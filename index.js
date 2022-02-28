3750ee4c1bed98c79ef4b9b830553d5a439afb2d
casperdennijs <casper.dennijs@outlook.com>
2022-02-28 17:48:00 +0100

    ESLint geinstalleerd en opgezet

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ const express = require('express')
 const { engine } = require('express-handlebars');
 const app = express();
 
-app.use(express.static('public'))
-
 app.engine('.hbs', engine({
   extname: '.hbs',
-  defaultLayoyt: 'main'
+  defaultLayout: 'main'
 }));
 app.set('view engine', '.hbs');
 app.set("views", "./views");
 
 app.get('/', (req, res) => {
-    res.render('home');
+  res.render('home');
 });
 
 app.listen(3000);
