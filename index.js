9b0d462ee6b1652ba1a38a8567a5dbb3b0ae7c72
casperdennijs <casper.dennijs@outlook.com>
2022-03-04 10:20:34 +0100

    Registratie en wachtwoord vergeten pagina's gemaakt

--- index.js
+++ index.js
@@ -13,7 +13,19 @@ app.set('view engine', '.hbs');
 app.set("views", "./views");
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('sign-in', {'title': 'Inloggen | League Connect'});
+});
+
+app.get('/sign-in', (req, res) => {
+  res.render('sign-in', {'title': 'Inloggen | League Connect'});
+});
+
+app.get('/sign-up', (req, res) => {
+  res.render('sign-up', {'title': 'Registreren | League Connect'});
+});
+
+app.get('/forgot-password', (req, res) => {
+  res.render('forgot-password', {'title': 'Wachtwoord vergeten | League Connect'});
 });
 
 app.listen(3000);
