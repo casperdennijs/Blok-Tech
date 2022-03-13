c4e6e65aa9a1ab381fea086e007560ed4c9b8ed7
casperdennijs <casper.dennijs@outlook.com>
2022-03-13 22:09:13 +0100

    Client side errors netter opgemaakt

--- public/js/login.js
+++ public/js/login.js
@@ -4,7 +4,8 @@ form.addEventListener("submit", (event) => {
 	const inputs = event.target.querySelectorAll("input");
 	const errorLabel = event.target.querySelector("label#error");
 	if (!inputs[0].value || !inputs[1].value) {
-		errorLabel.innerHTML = "Error: Niet alle invoer velden zijn ingevuld!";
+        errorLabel.classList.add("error");
+		errorLabel.innerHTML = "<span>Error:</span> Niet alle invoer velden zijn ingevuld!";
 		inputs.forEach(element => {
 			element.classList.add("foute-input");
 		});
--- public/js/register.js
+++ public/js/register.js
@@ -4,7 +4,8 @@ form.addEventListener("submit", (event) => {
 	const inputs = event.target.querySelectorAll("input");
 	const errorLabel = event.target.querySelector("label#error");
 	if (!inputs[0].value || !inputs[1].value || !inputs[2].value || !inputs[3].value) {
-		errorLabel.innerHTML = "Error: Niet alle invoer velden zijn ingevuld!";
+		errorLabel.classList.add("error");
+		errorLabel.innerHTML = "<span>Error:</span> Niet alle invoer velden zijn ingevuld!";
 		inputs.forEach(element => {
 			element.classList.add("foute-input");
 		});
@@ -12,7 +13,8 @@ form.addEventListener("submit", (event) => {
 		if (inputs[2].value === inputs[3].value) {
 			event.target.submit();
 		} else {
-			errorLabel.innerHTML = "Error: Wachtwoorden komen niet overeen met elkaar!";
+			errorLabel.classList.add("error");
+			errorLabel.innerHTML = "<span>Error:</span> Wachtwoorden komen niet overeen met elkaar!";
 			inputs[0].classList.remove("foute-input");
 			inputs[1].classList.remove("foute-input");
 			inputs[2].classList.add("foute-input");
