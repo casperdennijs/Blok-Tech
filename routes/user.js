366134e647df6aae6d39ed03881dff9f04b5d7be
casperdennijs <casper.dennijs@outlook.com>
2022-03-13 16:01:30 +0100

    Wachtwoord encrypted bij registreren en kan nu bij inloggen uitgelezen worden

--- routes/user.js
+++ routes/user.js
@@ -4,40 +4,57 @@ const User = require("../models/users");
 const bodyParser = require("body-parser");
 router.use(bodyParser.urlencoded({ extended: true }));
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 let session;
 
-/* Inloggen (creates session) */
+/* Inloggen (checks encrypted password & creates session) */
 router.post("/login", async (req, res) => {
-    User.findOne({ username: req.body.username, password: req.body.password }, (error, user) => {
-        if (error) {
-            console.log(error);
-            return res.status(500).redirect('/sign-in');
-        }
-        if (!user) {
+    try {
+        const getUser = await User.findOne({ username: req.body.username });
+        if (getUser) {
+          const comparePassword = await bcrypt.compare(req.body.password, user.password);
+          if (comparePassword) {
+            console.log("Succesvol ingelogd!");
+            session = req.session;
+            session.username = req.body.username;
+            return res.status(200).redirect('/');
+          } else {
+            console.error("Verkeerde gebruikersnaam of wachtwoord!");
+            return res.status(404).redirect('/sign-in');
+          }
+        } else {
+            console.error("Verkeerde gebruikersnaam of wachtwoord!");
             return res.status(404).redirect('/sign-in');
         }
-        session = req.session;
-        session.username = req.body.username;
-        return res.status(200).redirect('/');
-    });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).redirect('/sign-in');
+    }
 });
 
-/* Registreren */
+/* Registreren (encrypts password) */
 router.post("/register", async (req, res) => {
-    const newUser = new User({
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+    const createUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     })
-    newUser.save((error) => {
+
+    createUser.save((error) => {
         if (error) {
-            console.log(error);
+            console.error(error);
             return res.status(500).redirect('/sign-up');
+        } else {
+            console.log("Account aangemaakt!")
+            return res.status(200).redirect('/profile-setup');
         }
-        return res.status(200).redirect('/profile-setup');
     });
 });
 
+/* Uitloggen (destroys session) */
 router.post('/logout', (req, res) => {
     req.session.destroy();
     res.redirect('/sign-in');
